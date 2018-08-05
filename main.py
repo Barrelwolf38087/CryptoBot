b6583fedcd454793c2c055fc6b51742ad65f94bb
= <=>
2018-08-05 19:30:22 -0400

    Preliminary work done; bot is functional

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-import discord
-from discord.ext import commands as cmds
+import discord, asyncio, platform
+from discord.ext.commands import Bot
+from discord.ext import commands
 
-bot = cmds.Bot(command_prefix='/', case_insensitive=True, pm_help=True, description='A bot for comparing cryptocurrenct values.')
+TOKEN = "NDY0MTYyMDk3ODI4MzMxNTIx.DkiwIw.PWSRmOKmL07_8LmR1EjzvGlfguw"
+
+#client = discord.Client()
+
+bot = Bot(command_prefix='!', case_insensitive=True, pm_help=True, description='A bot for comparing cryptocurrenct values.')
+
+@bot.event
+async def on_ready():
+    print(
+        "Logged in\n"
+        + str(bot.user.name) + "\n"
+        + str(bot.user.id)
+    )
+
+@bot.command()
+async def get(ctx):
+    await ctx.send("Nothing to see here yet!")
+
+bot.run(TOKEN)
