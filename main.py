afd9889147f1b712ae4339735e1063199b3dbb0f
= <=>
2018-08-06 15:50:53 -0400

    Logic done, bot works as intended.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import discord, asyncio, platform
+import discord, requests, asyncio, platform
 from discord.ext.commands import Bot
-from discord.ext import commands
+
+coin_list = list(requests.get("https://www.cryptocompare.com/api/data/coinlist/").json()['Data'].keys())
+coin_list.append('USD')
 
 # Place your token in a file called "token.txt"
 token_file = open("token.txt", "r")
 TOKEN = token_file.read()
 
-bot = Bot(command_prefix='!', case_insensitive=True, pm_help=True, description='A bot for comparing cryptocurrenct values.')
+bot = Bot(command_prefix='!', case_insensitive=True, pm_help=True, description='A bot for comparing cryptocurrency values.')
+bot.remove_command("help")
 
 @bot.event
 async def on_ready():
@@ -17,7 +20,24 @@ async def on_ready():
     )
 
 @bot.command()
-async def get(ctx):
-    await ctx.send("Nothing to see here yet!")
+async def help(ctx):
+    await ctx.send(
+        "\nExample: Getting the price of a Bitcoin in USD:\n" +
+        "`  !price USD BTC`"
+    )
+
+@bot.command()
+async def price(ctx, to_sym, from_sym):
+    if to_sym in coin_list and from_sym in coin_list:
+        res = requests.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms="+from_sym+"&tsyms="+to_sym)
+        await ctx.send("Price of 1 " + from_sym + ": " + str(res.json()[from_sym][to_sym]) + " " + to_sym)
+        await ctx.send("Data from <https://www.cryptocompare.com>")
+    else:
+        if to_sym not in coin_list:
+            await ctx.send("Error: " + to_sym + " not a valid symbol. Please check your typing.")
+        if from_sym not in coin_list:
+            await ctx.send("Error: " + from_sym + " not a valid symbol. Please check your typing.")
+        await ctx.send("Please note that USD is the only non-cryptocurrency accepted.")
+
 
 bot.run(TOKEN)
